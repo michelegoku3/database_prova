80b8560425e708e4579885d59f1f208dcff3f922
michelegoku3 <143042491+michelegoku3@users.noreply.github.com>
2023-10-21 00:35:08 +0200

    Update connetti.php

--- connetti.php
+++ connetti.php
@@ -1,21 +1,32 @@
 <?php
-
 $servername = "localhost";
 $username = "migo3";
 $password = "";
-$database = "my_migo3";
+$dbname = "my_migo3";
 
-// Crea una connessione
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifica la connessione
 if ($conn->connect_error) {
     die("Connessione fallita: " . $conn->connect_error);
 }
 
-echo "Connessione riuscita al database";
+$materia = $_GET['materia'];
 
-// Chiudi la connessione
+$sql = "SELECT nome, cognome, $materia FROM TRACK_Studente";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $rows = array();
+
+    while ($row = $result->fetch_assoc()) {
+        $rows[] = $row;
+    }
+
+    echo json_encode($rows);
+} else {
+    echo "Nessun risultato";
+}
+
 $conn->close();
-
 ?>
